34ca1b158077c27fc268e54d60fa6ddbce71f890
Mikaely <miihgama2404@gmail.com>
2026-05-14 22:10:13 -0300

    14/05
    14/05

---
+++ src/controllers/testController.js
@@ -0,0 +1,31 @@
+const connection = require("../database/connection")
+
+const testConnection = async (req, res) => {
+    try {
+        await connection.raw("SELECT 1+1 as result")
+
+        return res.json({ message: "Banco conectado com sucesso :)" })
+    } catch (error) {
+        return res.status(500).json({
+            message: "Erro ao conectar ao banco :(",
+            error: error
+        })
+    }
+}
+
+const listTables = async (req, res) => {
+    try {
+        const tables = await connection.raw(
+            "SELECT name FROM sqlite_master WHERE type = 'table'"
+        )
+
+        return res.json(tables)
+    } catch (error) {
+        return res.status(500).json(error)
+    }
+}
+
+module.exports = {
+    testConnection,
+    listTables
+}
